e391ffec0123f39b3c00c3e9725230c59d2857a3
Josh Lee <jpal.lee7@gmail.com>
2018-11-14 08:27:21 -0800

    Removed test_config check

--- shortener/__init__.py
+++ shortener/__init__.py
@@ -6,6 +6,8 @@ from shortener import (
 )
 
 
+
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
@@ -13,9 +15,6 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
-
-    if test_config is not None:
-        app.config.update(test_config)
 
     #ensure the instance folder exists
     try:
