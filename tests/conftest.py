01dc7375d68b7d74958e540833b8c2a7fee087f3
Josh Lee <jpal.lee7@gmail.com>
2018-12-06 21:30:39 -0800

    Added auth actions to conftest for pytest module

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,3 +24,21 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='testing', password='testing'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
