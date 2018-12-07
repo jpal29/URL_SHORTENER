f3bf3c0f7c9bf76aad08f768c196724fa5257f29
Josh Lee <jpal.lee7@gmail.com>
2018-12-06 21:34:33 -0800

    Added test content case for home page and for status response for /shorten route

--- tests/test_shorten.py
+++ tests/test_shorten.py
@@ -2,4 +2,9 @@ import pytest
 
 def test_index(client):
     response = client.get('/')
-    assert b"Index Page" in response.data
+    assert b"The one stop shop for url shortening." in response.data
+
+
+def test_url_shorten(client, auth, app):
+    auth.login()
+    assert client.get('/shorten').status_code == 200
