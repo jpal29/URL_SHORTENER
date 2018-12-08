eb9bcd7842966a7c96fa59feac6961cc89328918
Josh Lee <jpal.lee7@gmail.com>
2018-12-08 10:30:18 -0800

    Added a test case. Also testing travis integration

--- tests/test_shorten.py
+++ tests/test_shorten.py
@@ -6,6 +6,7 @@ from shortener.db import get_db
 
 def test_index(client):
     response = client.get('/')
+    assert response.status_code == 200
     assert b"Login" in response.data
     assert b"Register" in response.data
     assert b"The one stop shop for url shortening." in response.data
